519a35afc653d1163e2e5027e85f6838fa2f5186
Manlio Joaquín García González <info@manliogarcia.es>
2020-01-23 15:39:51 +0000

    Add “inspect” action

---
+++ lib/bach/actions.d/inspect.sh
@@ -0,0 +1,11 @@
+set_action "inspect" "project_name instance_name"                              \
+"Run an interactive bash shell instead of entrypoint, in an alternate container." \
+"Usage:"                                                                       \
+"  %program_name% %action_name% [%project_name%] [%instance_name%]"            \
+""
+
+inspect(){(
+    cd "$($0 locate "$project_name" "$instance_name")"
+    docker-compose run --rm -u 0 --entrypoint /bin/bash "$project_name"
+    exit $?
+)}
