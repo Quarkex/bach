eb5655bc6e282b8717296bffaad07376d205451a
Manlio Joaquín García González <quarkex@pm.me>
2020-11-09 15:26:35 +0100

    Add really common compose actions as first-class actions

---
+++ lib/bach/actions.d/logs.sh
@@ -0,0 +1,34 @@
+set_action "logs" "project_name instance_name"                                 \
+"Send a docker-compose ”logs -f --tail 10” action to an instance of a project." \
+"Usage:"                                                                       \
+"  %program_name% %action_name% %project_name% %instance_name% [arguments]"    \
+""
+
+logs(){(
+
+    composed_options=""
+    for option in "${!options[@]}"; do
+        if [ ! ${!option} == false ]; then
+            [ ${#option} -eq 1 ] && flag="-$option" || flag="--$option"
+            if [ ${!option} == true ]; then
+                composed_options="$compose_flags $flag"
+            else
+                if [ ! ${!option} == false ]; then
+                    composed_options="$composed_options $flag ${!option}"
+                fi
+            fi
+        fi
+    done
+
+    target_folder="$instances_folder/$project_name.d"
+
+    instance_folder="$target_folder/$instance_name"
+    if [ ! -d "$instance_folder" ]; then
+        echo "ERROR: Project $instance_name does not exist.";
+        return -1;
+    else
+        cd "$instance_folder";
+        docker-compose logs -f --tail 10 $@ $composed_options;
+        return $?
+    fi
+)}
